82b0d53b08a105e05679f28ad1afaffe94c93db5
ghrapan <gena@entrypoint.lv>
2021-07-12 17:10:01 +0300

    Enablind Swagger for production.

--- src/SampleAPI/Startup.cs
+++ src/SampleAPI/Startup.cs
@@ -63,9 +63,10 @@ namespace SampleAPI
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SampleAPI v1"));
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SampleAPI v1"));
 
             app.UseHttpsRedirection();
 
